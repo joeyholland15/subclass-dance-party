08885a171e6184429f35b6f78477f780b1e2ce5a
joeyholland15 <joeyholland15@gmail.com>
2015-12-01 12:04:21 -0800

    Added animation for square dancers

--- src/firstDancer.js
+++ src/firstDancer.js
@@ -6,22 +6,33 @@ var firstDancer = function(top, left, timeBetweenSteps){
   this.setPosition();
 };
 
-firstDancer.prototype = Object.create(makeDancer.prototype);
+firstDancer.prototype = Object.create(makeDancer.prototype); 
 
 firstDancer.prototype.step = function(){
   makeDancer.prototype.step.call(this);
-  if(this.position === 1){
-    this.top -= 50;
-    this.position ++;
-  } else if (this.position === 2){
-    this.left += 50;
-    this.position ++;
-  } else if (this.position === 3){
-    this.top += 50;
-    this.position ++;
-  } else {
-    this.left -= 50;
-    this.position = 1;
-  }
-  this.setPosition();
+  //console.log(this.position)
+  var node = this;
+  //if(this.position === 1){
+  // if(!this.status) {
+     this.$node.animate({'top':'-=50'});
+  //   (function(){console.log('this ran')})()
+  //   this.status = true; 
+  // } else {
+    this.$node.animate({'top':'+=50'});
+    // console.log('this also');
+    // this.status = false; 
+  //}    
+  //}
+  //   this.position ++;
+  // } else if (this.position === 2){
+  //   this.$node.animate({'left': '+= 50'}, this.timeBetweenSteps)
+  //   this.position ++;
+  // } else if (this.position === 3){
+  //   this.$node.animate({'top': '+= 50'}, this.timeBetweenSteps)
+  //   this.position ++;
+  // } else {
+  //   this.$node.animate({'left': '-= 50'}, this.timeBetweenSteps)
+  //   this.position = 1;
+  // }
+  // this.setPosition()
 };
